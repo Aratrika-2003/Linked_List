939a6691dec1a23cb9dd277c39623e226b66a1af
Aratrika Mallick <95929714+Aratrika-2003@users.noreply.github.com>
2022-05-09 18:42:17 +0530

    Update llm1.c

--- llm1.c
+++ llm1.c
@@ -1,10 +1,75 @@
 #include"ll1.h"
 
 int main()
-{
+{   
     node* head = NULL;
+    int i,data,n,m,p;
+    char ch = 'y';
 
-    append(&head, 1);
+    printf("\nMenu : \n");
+    printf("\n---------\n");
+    printf("1.Append\n2.Traverse\n3.Add before\n4.Add after\n5.Delete\n6.Print in reverse order\n7.reverse the linked list\n");
+    
+    while(ch == 'y')
+    {
+        printf("\nEnter your choice : ");
+        scanf("%d",&i);
+
+        switch(i)
+        {
+            case 1: 
+                printf("\nenter a number to be appended ");
+                scanf("%d",&data);
+                
+                append(&head,data);
+                traverse();
+            break;
+
+            case 2:
+                printf("\nThe list of elements \n");
+                
+                traverse();
+
+            break;
+
+            case 3:
+                printf("\nEnter the number to be added before ");
+                scanf("%d",&n);
+
+                add_before(&head,n);
+
+                traverse();
+
+            break;
+
+            case 4:
+                printf("\nEnter the number to be added after ");
+                scanf("%d",&m);
+
+                add_after(head->next,m);
+
+                traverse();
+
+            break;
+
+            case 5:
+                delete(head);
+
+                traverse();
+            break;
+
+            case 6:
+                print_reverse(&head);
+
+            break;
+
+            case 7:
+                rec_rev(head);
+
+            break;
+        }
+    }
+    /*append(&head, 1);
 
     add_before(&head,2);
 
@@ -14,7 +79,7 @@ int main()
     
     add_after(head->next,5);
 
-    traverse(head);
+    traverse(head);*/
 
     return 0;
 }
